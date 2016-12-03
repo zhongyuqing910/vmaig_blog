f1630b4037c0511e54d2a400279ea9416917fb43
505011923@qq.com <505011923@qq.com>
2016-12-03 14:49:57 +0800

    数据库链接信息修改

--- vmaig_blog/settings.py
+++ vmaig_blog/settings.py
@@ -81,12 +81,26 @@ WSGI_APPLICATION = 'vmaig_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME':'vmaig_blog',
+    'USER': 'root',
+    'PASSWORD': 'new_password',
+    'HOST': '',
+    'PORT': '',
     }
 }
+
+
 
 
 # Internationalization
